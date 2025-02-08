c61a45ed7006e4217e7fe136fcbf0740570fc202
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-02-08 22:38:57 +0800

    Update task_management.py

--- task_scheduling/stopit/task_management.py
+++ task_scheduling/stopit/task_management.py
@@ -6,54 +6,33 @@ from ..common import logger
 
 class TaskManager:
     def __init__(self):
-        self.data: Dict[str, Dict[str, Any]] = {}
+        self.tasks: Dict[str, Dict[str, Any]] = {}
 
-    def add(self, task_control: Any, skip_ctx: Any, task_id: str) -> None:
+    def add(self, cancel_obj: Any, skip_obj: Any, terminate_obj: Any, task_id: str) -> None:
         """
-        Add task_control, skip_ctx, and task_id to the dictionary.
+        Add task control objects to the dictionary.
 
-        :param task_control: An object that has a stop method.
-        :param skip_ctx: An object that has a skip method.
-        :param task_id: A string to be used as the key in the dictionary.
+        :param cancel_obj: An object that has a cancel method.
+        :param skip_obj: An object that has a skip method.
+        :param terminate_obj: An object that has a terminate method.
+        :param task_id: Task ID, used as the key in the dictionary.
         """
-        self.data[task_id] = {
-            'task_control': task_control,
-            'skip_ctx': skip_ctx
+        if task_id in self.tasks:
+            logger.warning(f"Task with task_id '{task_id}' already exists, overwriting")
+        self.tasks[task_id] = {
+            'cancel': cancel_obj,
+            'skip': skip_obj,
+            'terminate': terminate_obj
         }
-
-    def force_stop(self, task_id: str) -> None:
-        """
-        Call the stop method of the corresponding task_control instance using the task_id.
-
-        :param task_id: A string to be used as the key in the dictionary.
-        """
-        if task_id in self.data:
-            task_control = self.data[task_id]['task_control']
-            try:
-                task_control.stop()
-            except Exception as error:
-                logger.error(error)
-        else:
-            logger.warning(f"No task found with task_id '{task_id}', operation invalid")
-
-    def force_stop_all(self) -> None:
-        """
-        Call the stop method of all task_control instances in the dictionary.
-        """
-        for task_id, task_data in self.data.items():
-            try:
-                task_data['task_control'].stop()
-            except Exception as error:
-                logger.error(error)
 
     def remove(self, task_id: str) -> None:
         """
-        Remove the specified task_id and its associated data from the dictionary.
+        Remove the task and its associated data from the dictionary based on task_id.
 
-        :param task_id: A string to be used as the key in the dictionary.
+        :param task_id: Task ID.
         """
-        if task_id in self.data:
-            del self.data[task_id]
+        if task_id in self.tasks:
+            del self.tasks[task_id]
         else:
             logger.warning(f"No task found with task_id '{task_id}', operation invalid")
 
@@ -61,36 +40,72 @@ class TaskManager:
         """
         Check if the given task_id exists in the dictionary.
 
-        :param task_id: A string to be used as the key in the dictionary.
+        :param task_id: Task ID.
         :return: True if the task_id exists, otherwise False.
         """
-        return task_id in self.data
+        return task_id in self.tasks
 
-    def skip_task(self, task_id: str) -> None:
+    def _execute_operation(self, task_id: str, operation: str) -> None:
         """
-        Call the skip method of the corresponding skip_ctx instance using the task_id.
+        Execute the specified operation for the task_id.
 
-        :param task_id: A string to be used as the key in the dictionary.
+        :param task_id: Task ID.
+        :param operation: Operation type, can be 'cancel', 'skip', or 'terminate'.
         """
-        if task_id in self.data:
-            skip_ctx = self.data[task_id]['skip_ctx']
-            try:
-                skip_ctx.skip()
-            except Exception as error:
-                logger.error(error)
+        if task_id in self.tasks:
+            operation_obj = self.tasks[task_id][operation]
+            if operation_obj is not None:
+                try:
+                    getattr(operation_obj, operation)()
+                except Exception as error:
+                    logger.error(f"Failed to {operation} task with task_id '{task_id}': {error}")
+            else:
+                logger.warning(f"{operation} for task_id '{task_id}' is None, cannot execute {operation} operation")
         else:
             logger.warning(f"No task found with task_id '{task_id}', operation invalid")
 
-    def skip_all(self) -> None:
+    def cancel_task(self, task_id: str) -> None:
         """
-        Call the skip method of all skip_ctx instances in the dictionary.
+        Cancel the task based on task_id.
+
+        :param task_id: Task ID.
         """
-        for task_id, task_data in self.data.items():
-            try:
-                task_data['skip_ctx'].skip()
-            except Exception as error:
-                logger.error(error)
+        self._execute_operation(task_id, 'cancel')
 
+    def cancel_all_tasks(self) -> None:
+        """
+        Cancel all tasks in the dictionary.
+        """
+        for task_id in self.tasks:
+            self.cancel_task(task_id)
 
-# Create Manager instance
-task_manager = TaskManager()
+    def skip_task(self, task_id: str) -> None:
+        """
+        Skip the task based on task_id.
+
+        :param task_id: Task ID.
+        """
+        self._execute_operation(task_id, 'skip')
+
+    def skip_all_tasks(self) -> None:
+        """
+        Skip all tasks in the dictionary.
+        """
+        for task_id in self.tasks:
+            self.skip_task(task_id)
+
+    def terminate_task(self, task_id: str) -> None:
+        """
+        Terminate the task based on task_id.
+
+        :param task_id: Task ID.
+        """
+        self._execute_operation(task_id, 'terminate')
+
+    def terminate_all_tasks(self) -> None:
+        """
+        Terminate all tasks in the dictionary.
+        """
+        for task_id in self.tasks:
+            self.terminate_task(task_id)
+            print(task_id)
