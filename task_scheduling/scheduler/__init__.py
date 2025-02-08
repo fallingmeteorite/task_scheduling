6b81db87e8da131a8e6815785aae6bf84bedcb2b
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-02-08 11:22:13 +0800

    Update __init__.py

--- task_scheduling/scheduler/__init__.py
+++ task_scheduling/scheduler/__init__.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
-from .io_async_task import io_async_task
-from .io_liner_task import io_liner_task
+from .cpu_async_task import CpuAsyncTask
+from .cpu_liner_task import CpuLinerTask
+
+from .io_async_task import IoAsyncTask
+from .io_liner_task import IoLinerTask
+
+from .timer_task import TimerTask
+
+
 from .utils import *
 
+io_liner_task = IoLinerTask()
+io_async_task = IoAsyncTask()
+
+cpu_liner_task = CpuLinerTask()
+cpu_async_task = CpuAsyncTask()
+
+timer_task = TimerTask()
+
+detector = AwaitDetector()
