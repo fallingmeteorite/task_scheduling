e62addf06e61b7004d1b5aac6f54e81a098f6c11
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-02-08 22:38:30 +0800

    Update __init__.py

--- task_scheduling/__init__.py
+++ task_scheduling/__init__.py
@@ -9,8 +9,9 @@ from .config import ensure_config_loaded
 # Initialize the config dict
 ensure_config_loaded()
 
-from .queue_info_display import get_all_queue_info
+from .queue_info_display import get_tasks_info
 from .scheduler import *
 from .task_creation import task_creation
+from .scheduler_management import shutdown
 
 __version__ = "2.0.0"
