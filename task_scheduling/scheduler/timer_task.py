46a4332c05b67fb955b9a07f7c1fb377482bb680
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-02-09 22:04:33 +0800

    Update timer_task.py

--- task_scheduling/scheduler/timer_task.py
+++ task_scheduling/scheduler/timer_task.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# Author: fallingmeteorite
 import queue
 import threading
 import time
@@ -9,19 +11,34 @@ from typing import Callable, Dict, List, Tuple, Optional, Any
 from ..common import logger
 from ..config import config
 from ..manager import task_status_manager
-from ..stopit import TaskManager, skip_on_demand, StopException, ThreadingTimeout, TimeoutException
+from ..stopit import ThreadTaskManager, skip_on_demand, StopException, ThreadingTimeout, TimeoutException
 
 # Create Manager instance
-task_manager = TaskManager()
+task_manager = ThreadTaskManager()
 
 
 def _execute_task(task: Tuple[bool, str, str, Callable, Tuple, Dict]) -> Any:
+    """
+    Execute a task and handle its status.
+
+    Args:
+        task (Tuple[bool, str, str, Callable, Tuple, Dict]): A tuple containing task details.
+            - timeout_processing (bool): Whether timeout processing is enabled.
+            - task_name (str): Name of the task.
+            - task_id (str): ID of the task.
+            - func (Callable): The function to execute.
+            - args (Tuple): Arguments to pass to the function.
+            - kwargs (Dict): Keyword arguments to pass to the function.
+
+    Returns:
+        Any: Result of the task execution or error message.
+    """
     timeout_processing, task_name, task_id, func, args, kwargs = task
 
     return_results = None
     try:
-        task_status_manager.add_task_status(task_id, task_name, "running", time.time(), None, None,
-                                            timeout_processing)
+        task_status_manager.add_task_status(task_id, None, "running", time.time(), None, None,
+                                            None)
 
         logger.info(f"Start running timer task, task ID: {task_id}")
         if timeout_processing:
@@ -36,18 +53,18 @@ def _execute_task(task: Tuple[bool, str, str, Callable, Tuple, Dict]) -> Any:
         task_manager.remove(task_id)
     except TimeoutException:
         logger.warning(f"Timer task | {task_id} | timed out, forced termination")
-        task_status_manager.add_task_status(task_id, task_name, "timeout", None, None, None,
-                                            timeout_processing)
+        task_status_manager.add_task_status(task_id, None, "timeout", None, None, None,
+                                            None)
         return_results = "error happened"
     except StopException:
-        logger.warning(f"Timer task | {task_id} | was cancelled")
-        task_status_manager.add_task_status(task_id, task_name, "cancelled", None, None, None,
-                                            timeout_processing)
+        logger.warning(f"Timer task | {task_id} | cancelled, forced termination")
+        task_status_manager.add_task_status(task_id, None, "cancelled", None, None, None,
+                                            None)
         return_results = "error happened"
     except Exception as e:
         logger.error(f"Timer task | {task_id} | execution failed: {e}")
-        task_status_manager.add_task_status(task_id, task_name, "failed", None, None, e,
-                                            timeout_processing)
+        task_status_manager.add_task_status(task_id, None, "failed", None, None, e,
+                                            None)
         return_results = "error happened"
     finally:
         if task_manager.check(task_id):
@@ -57,41 +74,67 @@ def _execute_task(task: Tuple[bool, str, str, Callable, Tuple, Dict]) -> Any:
 
 class TimerTask:
     """
-    Linear task manager class, responsible for managing the scheduling, execution, and monitoring of linear tasks.
+    Timer task manager class, responsible for scheduling, executing, and monitoring timer-based tasks.
     """
     __slots__ = [
-        'task_queue', 'running_tasks', 'lock', 'condition', 'scheduler_lock',
+        'task_queue', 'running_tasks',
+        'lock', 'condition', 'scheduler_lock',
         'scheduler_started', 'scheduler_stop_event', 'scheduler_thread',
-        'idle_timer', 'idle_timeout', 'idle_timer_lock', 'task_results'
+        'idle_timer', 'idle_timeout', 'idle_timer_lock',
+        'task_results'
     ]
 
     def __init__(self) -> None:
+        """
+        Initialize the TimerTask manager.
+        """
         self.task_queue = queue.PriorityQueue()  # Task queue with priority based on execution time
         self.running_tasks = {}  # Running tasks
-        self.lock = threading.Lock()  # Lock to protect access to shared resources
-        self.scheduler_lock = threading.RLock()  # Thread unlock
 
+        self.lock = threading.Lock()  # Lock to protect access to shared resources
+        self.scheduler_lock = threading.RLock()  # Reentrant lock for scheduler operations
         self.condition = threading.Condition()  # Condition variable for thread synchronization
+
         self.scheduler_started = False  # Whether the scheduler thread has started
         self.scheduler_stop_event = threading.Event()  # Scheduler thread stop event
         self.scheduler_thread: Optional[threading.Thread] = None  # Scheduler thread
+
         self.idle_timer: Optional[threading.Timer] = None  # Idle timer
         self.idle_timeout = config["max_idle_time"]  # Idle timeout, default is 60 seconds
         self.idle_timer_lock = threading.Lock()  # Idle timer lock
+
         self.task_results: Dict[str, List[Any]] = {}  # Store task return results, keep up to 2 results for each task ID
 
     # Add the task to the scheduler
-    def add_task(self, delay: Optional[int] = None, daily_time: Optional[str] = None,
-                 timeout_processing: bool = False, task_name: str = "", task_id: str = "",
-                 func: Callable = None, *args, **kwargs) -> bool:
+    def add_task(self,
+                 delay: Optional[int],
+                 daily_time: Optional[str],
+                 timeout_processing: bool,
+                 task_name: str,
+                 task_id: str,
+                 func: Callable,
+                 *args,
+                 **kwargs) -> bool:
+        """
+        Add a task to the task queue.
+
+        Args:
+            delay (Optional[int]): Delay in seconds before the task should be executed (only once).
+            daily_time (Optional[str]): Specific time in "HH:MM" format for daily task execution.
+            timeout_processing (bool): Whether to enable timeout processing.
+            task_name (str): Task name (can be repeated).
+            task_id (str): Task ID (must be unique).
+            func (Callable): Task function.
+            *args: Positional arguments for the task function.
+            **kwargs: Keyword arguments for the task function.
+
+        Returns:
+            bool: Whether the task was successfully added.
+        """
         try:
             with self.scheduler_lock:
                 if self.scheduler_stop_event.is_set() and not self.scheduler_started:
                     self._join_scheduler_thread()
-
-                # Reduce the granularity of the lock
-                task_status_manager.add_task_status(task_id, task_name, "waiting", None, None, None,
-                                                    timeout_processing)
 
                 if delay is not None:
                     # Schedule task to run after a delay (only once)
@@ -108,7 +151,11 @@ class TimerTask:
                     logger.error(f"Timer task | {task_id} | no scheduling parameters provided")
                     return False
 
-                self.task_queue.put((timeout_processing, task_name, task_id, func, execution_time, args, kwargs))
+                # Reduce the granularity of the lock
+                task_status_manager.add_task_status(task_id, None, "waiting", None, None, None,
+                                                    None)
+
+                self.task_queue.put((execution_time, timeout_processing, task_name, task_id, func, args, kwargs))
 
                 if not self.scheduler_started:
                     self._start_scheduler()
@@ -120,7 +167,7 @@ class TimerTask:
 
                 return True
         except Exception as e:
-            logger.error(f"Timer task | {task_id} | error adding task: {e}")
+            logger.error(f"Error adding task | {task_id} |: {e}")
             return False
 
     # Start the scheduler
@@ -133,19 +180,21 @@ class TimerTask:
         self.scheduler_thread.start()
 
     # Stop the scheduler
-    def stop_scheduler(self, force_cleanup: bool, system_operations: bool = False) -> None:
+    def stop_scheduler(self,
+                       force_cleanup: bool,
+                       system_operations: bool = False) -> None:
         """
         Stop the scheduler thread.
 
         :param force_cleanup: If True, force stop all tasks and clear the queue.
                               If False, gracefully stop the scheduler (e.g., due to idle timeout).
-        :param system_operations: System execution metrics
+        :param system_operations: System execution metrics.
         """
         with self.scheduler_lock:
             # Check if all tasks are completed
             if not self.task_queue.empty() or not len(self.running_tasks) == 0:
                 if system_operations:
-                    logger.warning(f"Cpu asyncio task was detected to be running, and the task stopped terminating")
+                    logger.warning(f"Timer task was detected to be running, and the task stopped terminating")
                     return None
 
             if force_cleanup:
@@ -173,12 +222,15 @@ class TimerTask:
             self.idle_timer = None
             self.task_results = {}
 
+            logger.info(
+                "Scheduler and event loop have stopped, all resources have been released and parameters reset")
+
     # Scheduler function
     def _scheduler(self) -> None:
         """
         Scheduler function, fetch tasks from the task queue and submit them to the thread pool for execution.
         """
-        with ThreadPoolExecutor(max_workers=int(config["line_task_max"])) as executor:
+        with ThreadPoolExecutor(max_workers=int(config["timer_task"])) as executor:
             while not self.scheduler_stop_event.is_set():
                 with self.condition:
                     while self.task_queue.empty() and not self.scheduler_stop_event.is_set():
@@ -190,35 +242,45 @@ class TimerTask:
                     if self.task_queue.qsize() == 0:
                         continue
 
-                    timeout_processing, task_name, task_id, func, execution_time, args, kwargs = self.task_queue.get()
+                    execution_time, timeout_processing, task_name, task_id, func, args, kwargs = self.task_queue.get()
 
                     if execution_time > time.time():
                         self.task_queue.put(
-                            (timeout_processing, task_name, task_id, func, execution_time, args, kwargs))
+                            (execution_time, timeout_processing, task_name, task_id, func, args, kwargs))
                         self.condition.wait(execution_time - time.time())
                         continue
 
                 with self.lock:
-
                     future = executor.submit(_execute_task,
                                              (timeout_processing, task_name, task_id, func, args, kwargs))
                     self.running_tasks[task_id] = [future, task_name]
 
                 future.add_done_callback(
-                    partial(self._task_done, task_id, timeout_processing, task_name, task_id, func, args, kwargs))
+                    partial(self._task_done, task_id, timeout_processing, task_name, func, args, kwargs))
 
     # A function that executes a task
-    def _task_done(self, task_id: str, timeout_processing: bool, task_name: str, task_id_: str, func: Callable,
-                   args: Tuple, kwargs: Dict, future: Future) -> None:
+    def _task_done(self,
+                   task_id: str,
+                   timeout_processing: bool,
+                   task_name: str,
+                   func: Callable,
+                   args: Tuple,
+                   kwargs: Dict,
+                   future: Future) -> None:
         """
         Callback function after a task is completed.
 
         :param task_id: Task ID.
+        :param timeout_processing: Whether timeout processing is enabled.
+        :param task_name: Task name.
+        :param func: Task function.
+        :param args: Positional arguments for the task function.
+        :param kwargs: Keyword arguments for the task function.
         :param future: Future object corresponding to the task.
         """
         try:
             result = future.result()  # Get task result, exceptions will be raised here
-            if not result == "error happened":
+            if result != "error happened":
                 task_status_manager.add_task_status(task_id, None, "completed", None, time.time(), None, None)
         finally:
             # Ensure the Future object is deleted
@@ -227,15 +289,15 @@ class TimerTask:
                     del self.running_tasks[task_id]
 
             # Check if the task is a daily task and reschedule it for the next day
-            if not timeout_processing:
+            if daily_time := kwargs.get('daily_time'):
                 try:
-                    daily_time_obj = datetime.strptime(task_name, "%H:%M")
+                    daily_time_obj = datetime.strptime(daily_time, "%H:%M")
                     now = datetime.now()
                     scheduled_time = datetime(now.year, now.month, now.day, daily_time_obj.hour, daily_time_obj.minute)
                     if scheduled_time < now:
                         scheduled_time += timedelta(days=1)
                     execution_time = scheduled_time.timestamp()
-                    self.task_queue.put((timeout_processing, task_name, task_id_, func, execution_time, args, kwargs))
+                    self.task_queue.put((execution_time, timeout_processing, task_name, task_id, func, args, kwargs))
                 except ValueError:
                     # If task_name is not a valid time string, do not reschedule
                     logger.warning(
@@ -280,13 +342,15 @@ class TimerTask:
         if self.scheduler_thread and self.scheduler_thread.is_alive():
             self.scheduler_thread.join()
 
-    def force_stop_task(self, task_id: str) -> bool:
+    def force_stop_task(self,
+                        task_id: str) -> bool:
         """
         Force stop a task by its task ID.
 
-        :param task_id: task ID.
-        """
+        :param task_id: Task ID.
 
+        :return: bool: Whether the task was successfully force stopped.
+        """
         if not task_manager.check(task_id):
             logger.warning(f"Timer task | {task_id} | does not exist or is already completed")
             return False
@@ -298,3 +362,20 @@ class TimerTask:
             if task_id in self.task_results:
                 del self.task_results[task_id]
         return True
+
+    # Obtain the information returned by the corresponding task
+    def get_task_result(self,
+                        task_id: str) -> Optional[Any]:
+        """
+        Get the result of a task. If there is a result, return and delete the oldest result; if no result, return None.
+
+        :param task_id: Task ID.
+
+        :return: Optional[Any]: Task return result, or None if the task is not completed or does not exist.
+        """
+        if task_id in self.task_results:
+            result = self.task_results[task_id]
+            with self.lock:
+                del self.task_results[task_id]
+            return result
+        return None
