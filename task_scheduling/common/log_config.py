c1de428bb9c60d647bde596d92c5cea58519f835
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-02-09 22:04:10 +0800

    Update log_config.py

--- task_scheduling/common/log_config.py
+++ task_scheduling/common/log_config.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
+# Author: fallingmeteorite
 import sys
 
 from loguru import logger
 
 # Hardcoded log format for performance
+# The log format includes the timestamp, log level, name of the file and line number where the log was generated, and the log message
 DEFAULT_FORMAT: str = (
-    "<g>{time:YYYY-MM-DD HH:mm:ss}</g> "
-    "[<lvl>{level}</lvl>] "
-    "<c><u>{name}:{line}</u></c> | "
-    "{message}"
+    "<g>{time:YYYY-MM-DD HH:mm:ss}</g> "  # Timestamp, formatted as year-month-day hour:minute:second, displayed in green
+    "[<lvl>{level}</lvl>] "              # Log level, automatically colored based on the level
+    "<c><u>{name}:{line}</u></c> | "      # Name of the file and line number where the log was generated, displayed in cyan with underline
+    "{message}"                          # Log message
 )
 
 # Default log level
@@ -24,24 +26,24 @@ def configure_logger():
     """
     global _logger_configured
 
-    # Skip if logger is already configured
+    # Skip configuration if the logger is already set up
     if _logger_configured:
         return
 
-    # Remove all default handlers (only if necessary)
+    # Remove all default handlers if necessary
     logger.remove()
 
-    # Configure logger to output to console
+    # Configure logger to output to console with the specified format, level, and features
     logger.add(
         sys.stdout,
-        format=DEFAULT_FORMAT,
-        level=LOG_LEVEL,
-        colorize=True,
-        backtrace=True,
-        diagnose=True
+        format=DEFAULT_FORMAT,  # Use the predefined log format
+        level=LOG_LEVEL,        # Set the default log level
+        colorize=True,          # Enable log colorization
+        backtrace=True,         # Enable backtrace in case of exceptions
+        diagnose=True           # Enable diagnostic information in case of exceptions
     )
 
-    # Mark logger as configured
+    # Mark the logger as configured
     _logger_configured = True
 
 # Example log message (only for debugging)
