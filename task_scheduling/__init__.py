f1891e2f2ace8005668d1d3d4baeb40260e1e22f
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-02-10 16:29:31 +0800

    Update __init__.py

--- task_scheduling/__init__.py
+++ task_scheduling/__init__.py
@@ -4,13 +4,9 @@ from .common import configure_logger
 # Initialize logger configuration at module load
 configure_logger()
 
-from .config import ensure_config_loaded
+from .config import *
 
 # Initialize the config dict
 ensure_config_loaded()
-
-from .queue_info_display import get_tasks_info
-from .scheduler import *
-from .task_creation import task_creation, shutdown
 
 __version__ = "2.0.0"
