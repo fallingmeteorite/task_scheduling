946406c3a1d71c5e5081b91b09dd5a2c5e2e784b
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-06-19 21:10:47 +0800

    Update cpu_liner_task.py

--- task_scheduling/scheduler/cpu_liner_task.py
+++ task_scheduling/scheduler/cpu_liner_task.py
@@ -136,7 +136,7 @@ class CpuLinerTask:
                     task = self._task_status_queue.get(timeout=0.1)
                     status, task_id, task_name, start_time, end_time, error, timeout_processing = task
                     task_status_manager.add_task_status(task_id, task_name, status, start_time, end_time, error,
-                                                        timeout_processing)
+                                                        timeout_processing, "cpu_liner_task")
 
             except queue.Empty:
                 pass  # Ignore empty queue exceptions
