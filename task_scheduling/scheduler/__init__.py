2e5382d3cc5201a7732b5a9a78369dd3c665f7b6
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-06-23 14:58:40 +0800

    Update __init__.py

--- task_scheduling/scheduler/__init__.py
+++ task_scheduling/scheduler/__init__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 # Linear task section
-from .cpu_asyncio_task import CpuAsyncTask
+from .cpu_asyncio_task import CpuAsyncioTask
 from .cpu_liner_task import CpuLinerTask
 
 # Asynchronous task section
-from .io_asyncio_task import IoAsyncTask
+from .io_asyncio_task import IoAsyncioTask
 from .io_liner_task import IoLinerTask
 
 # Task timer
 from .timer_task import TimerTask
 
 io_liner_task = IoLinerTask()
-io_async_task = IoAsyncTask()
+io_asyncio_task = IoAsyncioTask()
 
 cpu_liner_task = CpuLinerTask()
-cpu_async_task = CpuAsyncTask()
+cpu_asyncio_task = CpuAsyncioTask()
 
 timer_task = TimerTask()
