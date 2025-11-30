38f279b3a30bf74dc51ad6015f80f33f6947bd9c
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-11-30 12:58:08 +0800

    Update variable.py

--- task_scheduling/variable.py
+++ task_scheduling/variable.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 # Author: fallingmeteorite
+"""Task scheduling constants.
+
+This module defines constants for task priorities and types,
+making them easily accessible for IDE autocompletion.
+"""
+
 # Task priority constants
-"""Common variables, so they can be searched by the IDE without having to type them yourself"""
 PRIORITY_LOW = priority_low = "low"
 PRIORITY_HIGH = priority_high = "high"
 
@@ -12,6 +17,7 @@ CPU_LINER = "cpu_liner_task"
 IO_LINER = "io_liner_task"
 TIMER = "timer_task"
 
+# Function type constants
 FUNCTION_TYPE_IO = "io"
 FUNCTION_TYPE_CPU = "cpu"
 FUNCTION_TYPE_TIMER = "timer"
